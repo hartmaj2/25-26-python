afe81fd99893a46932608b6e96fe249a404a5c91
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-10-10 14:06:29 +0200

    Add riddle

---
+++ Files/25_10_10/0Hadanky/0indentace.py
@@ -0,0 +1,16 @@
+# Co kod nize udela?
+# Co je v nasledujicim kodu za chybu?
+
+character = "Chuck Norris"
+health = 100
+print(f"Character: {character}")
+print(f"Starting Health: {health}")
+
+# Check if the character has enough health
+if health > 50:
+    print("You are in great shape!")
+print("With this much HP you can go fight a dragon!")
+if health <= 50:
+    print("You need to find a healer!")
+
+print("Let's start the adventure!")
