f80ca2443442ff604c1e8b861fc2b4ed71e97e5a
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-10-31 12:56:50 +0100

    Add exercises for lists

---
+++ Files/25_10_31/1Seznamy/1cviceni_seznamy.py
@@ -0,0 +1,32 @@
+# TODO: 
+# nize mas seznam itemu, ktere nabizi obchod 
+store_items = ["mana potion","sword","armor","shield"]
+
+# tvuj inventar
+inventory = []
+
+
+# TODO: vypis vsechny predmety, ktere obchod nabizi
+
+# OCEKAVANY VYSTUP PROGRAMU:
+# Items in store:
+#   1. mana potion
+#   2. sword
+#   3. armor
+#   4. shield
+
+
+# TODO: kup si druhy predmet tim
+# 1. odeberes ten predmet ze seznamu predmetu `store_items` a pridas do sveho
+# 2. pridas tento predmet do seznamu `inventory`
+# POZOR: tvuj program by mel fungovat i v pripade, kdyz se predmety v obchodu zmeni
+
+
+# TODO: Nech uzivatele zvolit cislo predmetu, ktery chce koupit a pridej mu ten predmet do inventare
+
+
+# BONUS TODO: hezky design tohoto programu by byl takovy, 
+# ze pro vytisknuti veci v obchode bychom si napsali funkci
+# tim padem budeme moct opakovane pouzivat tuto funkci
+# a nemusime tedy psat cely kod, ktery funkce provadi znovu a znovu
+
---
+++ Files/25_10_31/1Seznamy/2tezsi_seznamy.py
@@ -0,0 +1,38 @@
+# TODO: nize mas seznam itemu, ktere nabizi obchod a pod nim seznam jejich cen ve stejnem poradi
+store_items = ["potion","sword","armor","shield"]
+store_items_prices = [10,5,13,17]
+
+# tvuj inventar a seznam cen predmetu v tvem inventari (zatim nic nemas)
+inventory = []
+inventory_prices = []
+
+# pocet penez, ktere mas k dispozici
+money = 15
+
+# TODO: vypis vsechny predmety, ktere obchod nabizi a jejich ceny
+# Items in store: 
+#   1. potion     10
+#   2. sword      5
+#   3. armor      13
+#   4. shield     17
+
+
+# TODO: nech uzivatele zvolit poradi predmetu, ktery chce koupit a pridej mu ten predmet do inventare
+# POZOR: predmet lze koupit pouze pokud ma hrac dost penez
+
+
+
+# TODO: pridej i moznost buy a sell
+# 1. zeptej se uzivatele, zda chce kupovat nebo prodavat
+# 2. podle jeho volby:
+    # pokud si zvolil nakup -> zobraz seznam predmetu v obchode
+    #   ...          prodej -> zobraz seznam predmetu z inventare
+
+    
+
+# BONUS2 TODO: pridej moznost nechat si vypsat celkovou hodnotu vsech veci ve tvem inventari
+
+
+
+
+
