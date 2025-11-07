02114e1922de8be8d6c926878bbc98b17461dc2e
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-11-07 13:47:37 +0100

    Add intro code

---
+++ Files/25_11_07/NewConcepts/functions_intro.py
@@ -0,0 +1,30 @@
+# TODO: What if I want to instead print: (or in different order)
+# ------------------------
+# Health: xxx
+# Gold: xxx
+# Power: xxx
+# ------------------------
+
+hp = 100
+gold = 0
+power = 0
+
+print("Player stats")
+print("------------------------")
+print("HP:",gold)
+print("GLD:",gold)
+print("POW:",power)
+print("------------------------")
+
+hp = hp - 10
+gold = gold + 15
+power = power + 1
+input() # do something in the game
+
+print("Player stats")
+print("------------------------")
+print("HP:",gold)
+print("GLD:",gold)
+print("POW:",power)
+print("------------------------")
+
