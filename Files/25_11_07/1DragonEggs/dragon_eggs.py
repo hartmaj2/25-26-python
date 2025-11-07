3ede8ee8ec1a026b141fab1e0d3098e86e72eacf
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-11-07 11:36:48 +0100

    Add exercise for lists

---
+++ Files/25_11_07/1DragonEggs/dragon_eggs.py
@@ -0,0 +1,5 @@
+# We have an inventory with objects. Some of them are eggs.
+
+inventory = ["potion","sword","egg","book","egg","helmet"]
+
+# TODO: Write code that evolves all the eggs into dragons or chickens (with 1/2 probability)
