037efd621838cfaf014a04d6769093c058a58307
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-11-07 11:36:34 +0100

    Add hero task list

---
+++ Files/25_11_07/2Hero/hero.py
@@ -0,0 +1,16 @@
+# Now we will program a hero program together
+# Hero has activities to do to choose from 
+# Hero has some stats - hp, power, money etc.
+# Each activity should somehow change the stats (decrease or increase)
+
+# TIP: the game should involve some randomness in order to be fun (usually what is unexpected is fun)
+
+# What is the goal of the game?
+
+# TODO: Create variables for hero stats
+
+# TODO: Create code that shows what is the current state of the hero (how much hp, money etc we have)
+
+# TODO: Create activities and write a list of them to the player
+
+# TODO: When player enters id of an activity, do something to the player stats
