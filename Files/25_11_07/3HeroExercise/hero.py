114267fac16ec949035544567d8ed50808243279
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-11-07 13:44:21 +0100

    Rename folders

--- Files/25_11_07/3HeroExercise/hero.py
+++ Files/25_11_07/3HeroExercise/hero.py
@@ -9,8 +9,20 @@
 
 # TODO: Create variables for hero stats
 
+
+money = 0
+hp = 0
+power = 0
+
 # TODO: Create code that shows what is the current state of the hero (how much hp, money etc we have)
 
+hp_text = "HP: "
+money_text = "Money: "
+power_text = "Power: "
+
+print(f"{hp_text:<10}{hp}")
+print(f"{money_text:10}{money}")
+print(f"{power_text:10}{power}")
 # TODO: Create activities and write a list of them to the player
 
 # TODO: When player enters id of an activity, do something to the player stats
