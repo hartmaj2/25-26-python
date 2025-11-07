ecab4ec13ffa2f0be964df94997ba5c062315e7a
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-11-07 08:54:28 +0100

    Update riddle

--- Files/25_11_07/0Hadanky/1hadanka_ruzne_cykly.py
+++ Files/25_11_07/0Hadanky/1hadanka_ruzne_cykly.py
@@ -1,28 +1,34 @@
 # Dokáží uhodnout, co jednotlivé části kódu vypíší?
 # vizualizovat v Thonny
 
-seznam = ["kocka","vlocka","cocka","ocka"]
+seznam = ["kočka","vločka","čočka","očka"]
 
 # --- 0 ---
-print("0.")
+print("\nNultý:")
+print("--------")
 i = 0
 while i < 4:
     print(i)
     i = i + 1
 
 # --- 1 ---
-print("1.")
+print("\nPrvní:")
+print("--------")
 i = 0
 while i < 4:
     print(seznam[i])
     i = i + 1
 
 # --- 2 ---
-print("2.")
+print("\nDruhý:")
+print("--------")
 for i in range(4):
     print(seznam[i])
 
 # --- 3 ---
-print("3.")
+print("\nTřetí:")
+print("--------")
 for prvek in seznam:
     print(prvek)
+
+# What if we add another thing
