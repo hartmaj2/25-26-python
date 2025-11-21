8680e05c15d86a619504ea9d7f5145c7486dbedb
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-11-21 14:22:46 +0100

    Add dice game

---
+++ Files/25_11_21/1Kostky/dve_kostky.py
@@ -0,0 +1,7 @@
+# Zadání: Tajuplná osoba v kápi nám nabídla následující hru
+# 1. Zaplatím jedno euro na vstup do hry
+# 2. Hodím dvěmi kostkami
+# 3. Pokud součet čísel na kostkách je x, tak vyhraju x korun (nejprve hrajeme pro x=7)
+# 4. Je ta hra výhodná? (Kdybych ji hrál 1000 krát, vydělal bych si nebo ne?)
+
+# TODO: Jak bychom si mohli ověřit, zda se nám hrát tuto hru vyplatí v dlouhodobém hledisku?
