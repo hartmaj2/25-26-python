5f8465dd6227aa0774818dbcee0c2f15f346ee17
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2026-02-20 13:57:50 +0100

    Add riddle

---
+++ Files/26_02_20/0riddle.py
@@ -0,0 +1,11 @@
+# How long did user take to respond
+# How can we measure how long it took?
+
+import time
+import random
+
+time.sleep(random.randint(2,8))
+
+input("Press enter quick!!!")
+
+print("Well done!")
