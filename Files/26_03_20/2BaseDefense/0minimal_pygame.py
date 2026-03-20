75ba6271a3b56ff2792e3d3831aa1ea1a78a4eec
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2026-03-20 08:18:10 +0100

    Add minimal pygame code

---
+++ Files/26_03_20/2BaseDefense/0minimal_pygame.py
@@ -0,0 +1,9 @@
+import pygame
+
+pygame.init() # do some magic
+
+screen = pygame.display.set_mode((300,200)) # create screen object
+
+while True:
+    for event in pygame.event.get(): # check if some events happened and loop through them
+        pass
