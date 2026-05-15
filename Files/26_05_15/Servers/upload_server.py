c6fc8047a91325b400e590947729202f4ab688be
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2026-05-15 13:56:57 +0200

    Simplify code

--- Files/26_05_15/Servers/upload_server.py
+++ Files/26_05_15/Servers/upload_server.py
@@ -1,22 +1,10 @@
 # simple code for a server which can be used to share files via local network
 
+# add attribute accept=".py" to accept only files with .py file extension
+
 from flask import Flask, request
 
 app = Flask(__name__)
-
-only_python = '''
-    <form method="post" enctype="multipart/form-data">
-        <input type="file" name="file" accept=".py">
-        <button>Upload</button>
-    </form>
-    '''
-
-accept_all = '''
-    <form method="post" enctype="multipart/form-data">
-        <input type="file" name="file">
-        <button>Upload</button>
-    </form>
-    '''
 
 @app.route("/", methods=["GET", "POST"])
 def upload():
@@ -25,6 +13,11 @@ def upload():
         file.save(file.filename)
         return "Uploaded"
 
-    return accept_all
+    return '''
+    <form method="post" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <button>Upload</button>
+    </form>
+    '''
 
 app.run(host="0.0.0.0", port=80) # the default port is 80
